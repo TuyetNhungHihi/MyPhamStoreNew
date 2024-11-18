aa91fc67b694e8ea0697919939ca1e2ea28aa985
Le Phuoc Hai <22130068@st.hcmuaf.edu.vn>
2024-11-19 00:15:40 +0700

    Link lại js cho brand-management

---
+++ src/main/webapp/static/js/brand-management.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let bar = document.getElementById("menu-toggle");
+  let sidebar = document.getElementById("sidebar");
+  let bodylabel = document.getElementById("body-label");
+
+  let toggleBar = () => {
+    sidebar.classList.toggle("active");
+  };
+
+  bodylabel.addEventListener("click", (event) => {
+    sidebar.classList.add("active");
+  });
+
+  bar.addEventListener("click", (event) => {
+    toggleBar();
+  });
+});
