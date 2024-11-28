4d116b4648bc4ca5dc4aec85b89892b803732933
TuyetNhungHihi <22130200@st.hcmuaf.edu.vn>
2024-11-28 11:02:12 +0700

    set lại layout customer

--- src/main/webapp/static/js/customer.js
+++ src/main/webapp/static/js/customer.js
@@ -108,3 +108,21 @@ document.addEventListener("DOMContentLoaded", () => {
     toggleBar();
   });
   });
+  
+document.addEventListener("DOMContentLoaded", () => {
+    let bar = document.getElementById("menu-toggle");
+  let sidebar = document.getElementById("sidebar");
+  let bodylabel = document.getElementById("body-label");
+  
+  let toggleBar = () => {
+    sidebar.classList.toggle("active");
+  };
+  
+  bodylabel.addEventListener("click", (event) => {
+    sidebar.classList.add("active");
+  });
+  
+  bar.addEventListener("click", (event) => {
+    toggleBar();
+  });
+  });
