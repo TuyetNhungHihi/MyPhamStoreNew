d755257a947e6ed19e4ed4ee7a17877bb4e1dca1
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2024-12-01 16:19:31 +0700

    update docx

--- src/main/java/vn/edu/hcmuaf/fit/myphamstore/HelloServlet.java
+++ src/main/java/vn/edu/hcmuaf/fit/myphamstore/HelloServlet.java
@@ -1,10 +1,12 @@
 package vn.edu.hcmuaf.fit.myphamstore;
 
 import java.io.*;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello")
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -12,14 +14,8 @@ public class HelloServlet extends HttpServlet {
         message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("frontend/home.html").forward(request, response);
     }
 
     public void destroy() {
