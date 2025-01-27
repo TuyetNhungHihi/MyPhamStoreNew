0989380f14ff435365e12d74201eb290e30eab6f
TuyetNhungHihi <22130200@st.hcmuaf.edu.vn>
2025-01-27 22:38:03 +0700

    lọc sp (còn lỗi)

--- src/main/java/vn/edu/hcmuaf/fit/myphamstore/dao/IProductDAO.java
+++ src/main/java/vn/edu/hcmuaf/fit/myphamstore/dao/IProductDAO.java
@@ -8,6 +8,8 @@ import vn.edu.hcmuaf.fit.myphamstore.model.ProductModel;
 import java.util.List;
 
 public interface IProductDAO extends GenericDAO<ProductModel> {
+    List<ProductModel> getFilteredProducts(String keyword, String[] categories, String[] brands, String priceRange, int currentPage, int pageSize, String orderBy);
+
     ProductModel getProductDetail(Long id);
     List<ProductModel> getProductsByCategory(Long categoryId);
     List<ProductModel> getLatestProductsByCategory(Long categoryId, int limit);
